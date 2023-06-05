9ad2223e5fa729e0cd492a8aae6ec62d3c4fc1b2
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2023-06-05 08:59:25 +0200

    feat: finishing LeapYear kata with ROP

--- LeapYear_ROP.Tests/LeapCheckerShould.cs
+++ LeapYear_ROP.Tests/LeapCheckerShould.cs
@@ -2,7 +2,7 @@
 
 public class LeapCheckerShould
 {
-    [Fact]
+    [Fact(DisplayName = "Success when year is 1996")]
     public void ReturnSuccessWithYearIfDivisibleBy4()
     {
         const int year = 1996;
@@ -11,12 +11,30 @@ public class LeapCheckerShould
         Assert.Equal(1996, success.Value);
     }
 
-    [Fact]
+    [Fact(DisplayName = "Failure when year is 1997")]
     public void ReturnFailureWithMessageIfNotDivisibleBy4()
     {
         const int year = 1997;
         var result = LeapChecker.IsLeap(year);
         var failure = Assert.IsAssignableFrom<Failure<int>>(result);
-        Assert.Equal("1997 is not a leap year!", failure.Message); 
+        Assert.Equal("1997 is not a leap year!", failure.Message);
+    }
+
+    [Fact(DisplayName = "Success when year is 1600")]
+    public void ReturnSuccessWhenYearIsDivisibleBy400()
+    {
+        const int year = 1600;
+        var result = LeapChecker.IsLeap(year);
+        var success = Assert.IsAssignableFrom<Success<int>>(result);
+        Assert.Equal(1600, success.Value);
+    }
+
+    [Fact(DisplayName = "Failure when year is 1800")]
+    public void ReturnFailureWhenYearIsDivisibleBy100ButNotBy400()
+    {
+        const int year = 1800;
+        var result = LeapChecker.IsLeap(year);
+        var failure = Assert.IsAssignableFrom<Failure<int>>(result);
+        Assert.Equal("1800 is not a leap year!", failure.Message);
     }
 }
--- LeapYear_ROP/LeapChecker.cs
+++ LeapYear_ROP/LeapChecker.cs
@@ -2,8 +2,11 @@
 
 public static class LeapChecker
 {
-    public static Result<int> IsLeap(int year)
-    {
-        return year % 4 is 0 ? Result<int>.Ok(year) : Result<int>.Fail($"{year} is not a leap year!");
-    }
+    public static Result<int> IsLeap(int year) =>
+        CheckYear(year) ? Result<int>.Ok(year) : Result<int>.Fail($"{year} is not a leap year!");
+
+    private static bool CheckYear(int year) =>
+        DivisibleBy(4, year) && (DivisibleBy(400, year) || !DivisibleBy(100, year));
+
+    private static bool DivisibleBy(int factor, int year) => year % factor is 0;
 }
